3a3e71397cca7c5f6e1f98a1d91ed13bc97c0a46
AaronBantay <aaronbantay@gmail.com>
2022-06-13 14:18:53 -0700

    Initial Commit: main, test

---
+++ main.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file main.cpp
+ * @author Aaron Bantay (https://github.com/abant006)
+ * @version 0.1
+ * @date 2022-06-13
+ *
+ * Main file which will hold the heart of the program; all the instructions that the user will "see"
+ *
+ */
+ 
+#include <iostream>
+#include <vector>
---
+++ test.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file test.cpp
+ * @author Aaron Bantay (https://github.com/abant006)
+ * @version 0.1
+ * @date 2022-06-13
+ *
+ * Test file which will hold all the tests I run for classes in the program
+ *
+ */
+ 
+#include <iostream>
+#include <vector>
