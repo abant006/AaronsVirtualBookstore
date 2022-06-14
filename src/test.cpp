ecdb40246899ae3bcf6371e9a7758ee76ed00a43
AaronBantay <aaronbantay@gmail.com>
2022-06-13 17:31:07 -0700

    Changed the comments

--- src/test.cpp
+++ src/test.cpp
@@ -1,8 +1,6 @@
 /**
  * @file test.cpp
  * @author Aaron Bantay (https://github.com/abant006)
- * @version 0.1
- * @date 2022-06-13
  *
  * Test file which will hold all the tests I run for classes in the program
  *
