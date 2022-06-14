b84521c3bbab3a09e186c914e6994e3e8be8d583
AaronBantay <aaronbantay@gmail.com>
2022-06-13 17:38:03 -0700

    Revised comments to StoreUser.h

--- headers/StoreUser.h
+++ headers/StoreUser.h
@@ -7,5 +7,7 @@
  * The StoreUser has a set of functions defined within this class and a few which are pure virtual (must be defined in the derived classes). Any subclasses will be able to have access
  * to the store in a certain way by using the functions defined within this class.
  * 
- * TLDR: This class serves as a way for derived classes to access the list of books within the shop. 
+ * TLDR: This class serves as a way for derived classes to access the list of books within the shop. In a way, this class is BOTH the user and the store. Holds the list of book entries
+ *       seen in a store, but has functions that modifies/accesses those entries like a user would.
+ * 
  */
