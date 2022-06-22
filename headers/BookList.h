5b263dde92fbdc05709d42a35a174b3812686ad6
AaronBantay <aaronbantay@gmail.com>
2022-06-21 22:11:50 -0700

    Added comments, parameters, and potential functions

--- headers/BookList.h
+++ headers/BookList.h
@@ -12,6 +12,7 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 // each node will be a book entry
 // each entry in the shop will have the book's ID number, name, author, publish date, price, and a BookEntry pointer storing the address of the next entry in the shop.
@@ -37,18 +38,31 @@ class BookList {
         // assignment operator
         void operator=(const BookList &cpy);
         // mutators
-        void push_back();
-        void push_front();
-        void remove();
+        void push_back(const std::string & , const std::string & , int , int );
+        void push_front(const std::string & , const std::string & , int , int );
+        void remove(int );
+        void remove(const std::string & );
         void selection_sort();
-        void insert_ordered();
+        void insert_ordered(const std::string & , const std::string & , int , int );
         // accessors
-        bool isDuplicate() const;
+        // note: SEE PRIVATE HELPER FUNCTION BELOW
+        bool in(int , const std::string & ) const; // is two parameters here the right move? would 2 in() functions be better?
 
+        // output operator somewhere for printing the list
+
+        // adds integer ID values to the IDList vector (meant for classes using this one, that are opening/reading from text file)
+        void initIDList(int );
 
     private:
         BookEntry *head;
         BookEntry *tail;
+
+        // private helper function that scans the vector to see if the ID has already been added before creating a node with a unique ID
+        // bool in(int );
+
+        // temporary measure: vector that stores all the ID's to check if one has been used already
+        // might be a bad solution but just adding for now
+        std::vector<int> IDList;
 };
 
 #endif //__BOOKLIST_H__
