4a44e84b3a2c0b6ee0fecea02ac64af7e4826c75
AaronBantay <aaronbantay@gmail.com>
2022-06-21 21:25:07 -0700

    Added constructors/destructors, and a few accessors/modifier declarations

--- headers/BookList.h
+++ headers/BookList.h
@@ -6,8 +6,12 @@
  * This class will declare a custom Linked List used to store each book in the shop
  * 
  */
+#ifndef __BOOKLIST_H__
+#define __BOOKLIST_H__
+
 
 #include <iostream>
+#include <string>
 
 // each node will be a book entry
 // each entry in the shop will have the book's ID number, name, author, publish date, price, and a BookEntry pointer storing the address of the next entry in the shop.
@@ -26,9 +30,25 @@ struct BookEntry {
 
 class BookList {
     public:
-        // TODO: INSERT FUNCTIONS
+        // constructors / destructors
+        BookList() : head(nullptr), tail(nullptr) {} // inline
+        BookList(const BookList &cpy); // copy constructor (might need const, idk)
+        ~BookList();
+        // assignment operator
+        void operator=(const BookList &cpy);
+        // mutators
+        void push_back();
+        void push_front();
+        void remove();
+        void selection_sort();
+        void insert_ordered();
+        // accessors
+        bool isDuplicate() const;
+
 
     private:
         BookEntry *head;
         BookEntry *tail;
 };
+
+#endif //__BOOKLIST_H__
