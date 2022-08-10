c7d59643ad321e465fa44258983f7e3ab0154994
AaronBantay <aaronbantay@gmail.com>
2022-08-10 12:14:40 -0700

    Created Admin class

---
+++ headers/Admin.h
@@ -0,0 +1,35 @@
+/**
+ * @file Admin.h
+ * @author Aaron Bantay (https://github.com/abant006)
+ * 
+ * This is the admin clas (subclass) to the StoreUser class. This will act as an admin user
+ * where they can do the same thing as a store user, but also have admin capabilties.
+ * 
+ * In addition to sorting and outputing and purchasing, they can also add entries, remove entries, 
+ * and modify any entires in the list. They also have a 50% discount to all items they purchase.
+ * 
+ */
+
+#ifndef __ADMIN_H__
+#define __ADMIN_H__
+
+#include "BookList.h"
+#include "StoreUser.h"
+#include <iostream>
+#include <vector>
+#include <string>
+
+// class declaration interface which inherits from store user
+// can do everything StoreUser can, but more
+class Admin : public StoreUser {
+    public:
+        // default constructor
+        Admin();
+
+    private:
+        // private data members exclusive to the Admin class
+        std::string UserName;
+        std::string Password;
+};
+
+#endif //__ADMIN_H__
---
+++ src/Admin.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file Admin.cpp
+ * @author Aaron Bantay (https://github.com/abant006)
+ * 
+ * The cpp file for Admin.h which defines each function declared in the interface
+ * 
+ */
+
+#include "../headers/Admin.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
